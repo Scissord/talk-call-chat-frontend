f116ffd69c28b980c9e2a9f80d3dd5f99f95faa6
nurdaulet <scissxrd@gmail.com>
2024-08-23 17:59:28 +0500

    build

--- src/views/create/index.js
+++ src/views/create/index.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import axios from 'axios';
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom';
 
 const Create = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+
+  const orderId = searchParams.get('orderId');
+  const phone = searchParams.get('phone');
+
+  useEffect(() => {
+    if(orderId && phone) {
+      handleCreateOrder();
+    };
+  }, [orderId, phone])
+
+  const handleCreateOrder = async () => {
+    await axios({
+      url: '/conversations',
+      method: 'post',
+      data: { orderId, phone }
+    })
+  };
+
   return (
-    <div>Create</div>
+    <>
+      {!orderId || !phone ? (
+        <div>Перейдите по ссылке с LeadVertex</div>
+      ) : (
+        <div>Create</div>
+      )}
+    </>
   )
 }
 
