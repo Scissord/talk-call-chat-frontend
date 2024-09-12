45ecde2789b6194692a378b6f70ba1142c922d5c
nurdaulet <scissxrd@gmail.com>
2024-09-12 11:59:48 +0500

    fix

---
+++ build/assets/index-BBw5vm1_.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-vMTDaE4K.js";const s=()=>r.jsx("div",{children:"index"});export{s as default};
--- build/assets/index-tjR85Rxs.js
+++ build/assets/index-tjR85Rxs.js
@@ -1,4 +1,4 @@
-import{r as l,V as p,f,l as v,m as j,j as e,n as b,o as N}from"./index-CWtzLFlG.js";const o={container:`
+import{r as l,V as p,f,l as v,m as j,j as e,n as b,o as N}from"./index-vMTDaE4K.js";const o={container:`
     flex w-[80%] lg:w-[60%] h-[80vh] lg:h-[70vh] border border-gray-300
     dark:border-neutral-200 rounded-xl shadow-2xl
     shadow-neutral-400 dark:shadow-none
