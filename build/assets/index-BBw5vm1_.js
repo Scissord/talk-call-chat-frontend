778bcb680d1c1ef52b5c58235bd5c3ae9228749f
nurdaulet <scissxrd@gmail.com>
2024-09-12 13:05:44 +0500

    build

--- build/assets/index-BBw5vm1_.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-vMTDaE4K.js";const s=()=>r.jsx("div",{children:"index"});export{s as default};
---
+++ build/assets/index-BO-iEmdb.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-T2KCJ41K.js";const s=()=>r.jsx("div",{children:"index"});export{s as default};
