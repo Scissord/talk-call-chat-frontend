2fb8df379751829ea6b676fd33cf15a309115c2d
nurdaulet <scissxrd@gmail.com>
2024-09-12 11:54:05 +0500

    fix

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -2,5 +2,5 @@
 //   ? 'https://www.restrain.pw/api'
 //   : 'http://localhost:5173/api';
 
-// export const baseUrl = 'https://restrain.pw:8080/api';
-export const baseUrl = 'http://31.128.41.42:8080/api';
+export const baseUrl = 'https://restrain.pw:8080/api';
+// export const baseUrl = 'http://31.128.41.42:8080/api';
