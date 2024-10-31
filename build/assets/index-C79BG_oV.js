cbf26683246654f3deb8ea504112f7b8e60cc632
nurdaulet <scissxrd@gmail.com>
2024-10-31 18:15:28 +0500

    build

--- build/assets/index-C79BG_oV.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-D2GVcpJF.js";const s=()=>r.jsx("div",{children:"index"});export{s as default};
---
+++ build/assets/index-NmBPfX3_.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CW1KQDE0.js";const s=()=>r.jsx("div",{children:"index"});export{s as default};
