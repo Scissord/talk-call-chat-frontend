71eb11d413054f7d7613259b2daec9dac556e223
nurdaulet <scissxrd@gmail.com>
2025-02-13 16:06:11 +0500

    fix

---
+++ build/assets/index-BCxL7rMj.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-pX7tpvWh.js";const s=()=>r.jsx("div",{children:"index"});export{s as default};
--- build/assets/index-D6AHJCnE.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BpLMN415.js";const s=()=>r.jsx("div",{children:"index"});export{s as default};
