a3ac600287daf11120c1f7acacec5eaf487696fd
nurdaulet <scissxrd@gmail.com>
2025-02-13 15:25:43 +0500

    fix

--- src/pages/Chats/blocks/Chat/CustomerInfo/Femiston.tsx
+++ src/pages/Chats/blocks/Chat/CustomerInfo/Femiston.tsx
@@ -23,7 +23,9 @@ const Femiston: FC<FemistonProps> = (props) => {
   const items = [
     { id: 'cert', label: '7.1 Сертификат', i: 1 },
     { id: 'image', label: '7.2 Фото отзыв', i: 1 },
+    { id: 'image', label: '7.2 Фото отзыв 2', i: 2 },
     { id: 'audio', label: '7.3 Аудио отзыв', i: 1 },
+    { id: 'audio', label: '7.3 Аудио отзыв 2', i: 2 },
   ];
 
   return (
