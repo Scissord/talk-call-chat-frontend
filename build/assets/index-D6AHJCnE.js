ba02085432dcab087346b283c48577bc74176717
nurdaulet <scissxrd@gmail.com>
2025-02-13 15:25:53 +0500

    fix

---
+++ build/assets/index-D6AHJCnE.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BpLMN415.js";const s=()=>r.jsx("div",{children:"index"});export{s as default};
--- build/assets/index-aLbmSJAj.js
+++ build/assets/index-aLbmSJAj.js
@@ -1,4 +1,4 @@
-import{r as l,V as m,c as f,n as j,o as v,j as e,p as b,q as N}from"./index-BclSp3pk.js";const o={container:`
+import{r as l,V as m,c as f,n as j,o as v,j as e,p as b,q as N}from"./index-BpLMN415.js";const o={container:`
     flex w-[80%] lg:w-[60%] h-[80vh] lg:h-[70vh] border border-gray-300
     dark:border-neutral-200 rounded-xl shadow-2xl
     shadow-neutral-400 dark:shadow-none
